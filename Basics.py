57144cd4d33994e5e26ef0c995f5e151d3a880b3
Ankit Pandey <ankitpan1411@gmail.com>
2026-06-20 23:00:04 +0530

    Some basics of python

---
+++ Basics.py
@@ -0,0 +1,62 @@
+
+a = int(input("Enter a number : "))
+b = int(input("Enter second number : "))
+
+print("number a is : ", a)
+print("number b is : ", b)
+print("sum is : ", (a) + (b))
+
+a = int(input("Enter a number :"))
+b = int(input("Enter a number :"))
+
+print("sum is : ", a + b)
+
+a = float(input("Enter a number :"))
+b = float(input("Enter a number :"))
+
+print("remainder is :", a%b)
+
+a = 34 
+b = 80
+print(a>b)
+
+a = float(input("Enter a number : "))
+b = float(input("Enter a number : "))
+
+print("Average is :" , (a+b)/2)
+
+a = float(input("Enter a number :"))
+print("sqare is :", a*a)
+
+a = str(input("Enter a name :")) 
+
+name = "Ankit"
+
+nameshart = name[0:3]
+print(nameshart)
+
+jh = name[2]
+print(jh)
+
+name = "kljadfsghiureiuhgq"
+print(len(name))
+print(name.endswith("dfg"))
+print(name.startswith("dfg"))
+print(name.capitalize())
+print(name.endswith("dfg"))
+
+
+
+
+a = str(input("Enter your name :"))
+print("Good Afternoon", a)
+
+letter = "Dear \nAnkit Pandey \n\t You are selected \n26/3/26"
+print(letter)
+
+a = "Ankit     Pandey"
+if"  " in a:
+    print("Double Space Found")
+else:
+    print("No Double Space")
+a = "Ankit   Pandey"
